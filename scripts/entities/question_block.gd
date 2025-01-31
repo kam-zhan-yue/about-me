9829c7e59e135af21f4446cf157003059cb77dae
Alex Kam <kamzhanyue@gmail.com>
2025-01-31 22:06:41 +0800

    feat: implement question block

---
+++ scripts/entities/question_block.gd
@@ -0,0 +1,24 @@
+class_name QuestionBlock
+extends Node2D
+
+@onready var sprite := $AnimatedSprite2D as AnimatedSprite2D
+
+var active := true
+signal hit
+
+func _ready() -> void:
+	sprite.play("idle")
+
+func _on_area_2d_area_entered(_area: Area2D) -> void:
+	if not active: return
+	hit.emit()
+	active = false
+	sprite.play("empty")
+	play_animation()
+
+func play_animation() -> void:
+	var tween = get_tree().create_tween()
+	var original_pos = position
+	tween.tween_property(self, "position", original_pos + Vector2(0, -5), 0.15).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "position", original_pos, 0.15).set_trans(Tween.TRANS_SINE)
+	pass
