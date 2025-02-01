a3954b132e530bab972ec9fb9f8be14e11391ef1
Alex Kam <kamzhanyue@gmail.com>
2025-02-01 15:20:21 +0800

    feat: implement player animations

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,14 +1,18 @@
 class_name Player
 extends CharacterBody2D
-
-@onready var sprite := $Mario as Sprite2D
+ 
+@onready var sprite := $Sprite as AnimatedSprite2D
 
 const FADE_TIME = 1.2
 const SPEED = 175.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -500.0
 const PLAYER_WIDTH = 18.0
 
 var interactive = true
+var facing_right = true
+
+func _ready() -> void:
+	sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,6 +25,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sprite.play("jump_start")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -32,7 +37,22 @@ func _physics_process(delta: float) -> void:
 	# Check if can move left
 	if velocity.x < 0 and not Game.in_bounds(position.x - PLAYER_WIDTH / 2):
 		velocity.x = 0
+	animate_player()
 	move_and_slide()
+
+func animate_player() -> void:
+	if facing_right and velocity.x < 0:
+		facing_right = false
+		sprite.flip_h = true
+	elif not facing_right and velocity.x > 0:
+		facing_right = true
+		sprite.flip_h = false
+	
+	if not is_on_floor() and velocity.y > 0:
+		sprite.play("jump_fall")
+	elif is_on_floor() and not velocity.y < 0:
+		var walking = velocity.length() > 0
+		sprite.play("walk" if walking else "idle")
 
 func fade_in():
 	sprite.modulate.a = 0.0
