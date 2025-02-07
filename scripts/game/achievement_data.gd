c35cf2c54b3bf7ae074443487cd4d611f324e823
Alex Kam <kamzhanyue@gmail.com>
2025-02-07 23:28:02 +0800

    feat: add label popup

--- scripts/game/achievement_data.gd
+++ scripts/game/achievement_data.gd
@@ -1,5 +1,22 @@
 class_name AchievementData
 extends Resource
 
+enum Tag {
+	Unity,
+	CSharp,
+	Godot,
+	GodotScript,
+	Bevy,
+	Rust,
+	React,
+	Typescript,
+	Javascript,
+	Django,
+	Python,
+	Spring,
+	Java,
+}
+
 @export var title := ""
+@export var tag: Array[Tag] = []
 @export var pages: Array[PageData] = []
