094fdd5316603ab2ef06c27a0c259993591d56b8
Alex Kam <kamzhanyue@gmail.com>
2025-02-08 21:24:31 +0800

    feat: add fading of game popup on achievement

--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -11,6 +11,8 @@ func init() -> void:
 	transition_popup.init()
 	Achievements.on_countdown_started.connect(_on_countdown_started)
 	Achievements.on_countdown_completed.connect(_on_countdown_completed)
+	Achievements.on_achievement.connect(_on_achievement)
+	Achievements.on_done_showing.connect(_on_done_showing)
 
 func _on_countdown_started() -> void:
 	await Global.fade_out(game_popup)
@@ -25,3 +27,9 @@ func transition_in() -> void:
 	
 func transition_out() -> void:
 	await transition_popup.fade_out()
+
+func _on_achievement(_achievement: AchievementData) -> void:
+	Global.fade_out(game_popup)
+
+func _on_done_showing() -> void:
+	Global.fade_in(game_popup)
