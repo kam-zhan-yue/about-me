cc56e92dbcc72e6cad88bddbfcc881c8b9196866
Alex Kam <kamzhanyue@gmail.com>
2025-02-11 23:39:55 +0800

    feat: stop timer on game end

--- scripts/ui/time_popup.gd
+++ scripts/ui/time_popup.gd
@@ -8,6 +8,7 @@ var time := 0.0
 
 func _ready() -> void:
 	Game.on_date_changed.connect(_start_timer)
+	Game.on_end_game.connect(_end_timer)
 
 func _process(delta: float) -> void:
 	if not self.started: return
@@ -18,3 +19,6 @@ func _process(delta: float) -> void:
 func _start_timer(_date: Date) -> void:
 	self.started = true
 	Game.on_date_changed.disconnect(_start_timer)
+
+func _end_timer() -> void:
+	self.started = false
